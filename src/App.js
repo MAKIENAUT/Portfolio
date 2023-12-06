88af03da1d1f66266dc8620e496645f5871580ea
MAKIENAUT <mcrayescoto@gmail.com>
2023-12-06 13:32:47 +0800

    PUSHED

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import './Components/Home/Home.css';
 import './Components/Navbar/Navbar.css';
 import HomeMain from './Components/Home/Home.js';
-import Navbar from './Components/Navbar/Navbar.js';
-import Projects from './Components/Projects/Projects.js';
 
 const App = () => {
 	return (
